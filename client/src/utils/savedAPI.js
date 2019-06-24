f6b46e2162f3d57fbbafa728fac2715cca4432f4
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-06-24 14:20:30 -0700

    APIs updated

--- client/src/utils/savedAPI.js
+++ client/src/utils/savedAPI.js
@@ -3,21 +3,21 @@ import axios from "axios";
 export default {
   // Gets all books
   getBooks: function() {
-    return axios.get("/api/books");
+    return axios.get("/api/saved");
   },
   // Gets the book with the given id
   viewBook: function(id) {
-    return axios.get("/api/books/" + id);
+    return axios.get("/api/saved/" + id);
   },
   // Deletes the book with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api/saved/" + id);
   },
   // Saves a book to the database
   saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+    return axios.post("/api/saved/", bookData);
   },
   getGoogleBooks: function(term) {
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + term + "&maxResults=25");
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + term);
   }
 };
